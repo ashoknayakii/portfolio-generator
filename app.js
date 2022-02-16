cd7c48e54671917af9992f37fe84c3eb7a32e8c9
Ashok Nayak <ashok.nayakii@gmail.com>
2022-02-16 18:24:35 -0500

    first question written in inquirer

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-console.log(profileDataArgs);
-
-const [name,github] = profileDataArgs;
-
-fs.writeFile('index.html', generatePage(name,github), err => { if (err) throw err;
-
-console.log('Portfolio complete! Check out index.html to see the output!');
-});
+inquirer 
+.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+    }
+])
+.then(answers => console.log(answers));
 
 
 
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('index.html', generatePage(name,github), err => { if (err) throw err;
+
+// console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
 
 
@@ -25,9 +32,6 @@ console.log('Portfolio complete! Check out index.html to see the output!');
 
 
 
-// const printProfileData = profileDataArr => {
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
 
-//   printProfileData(profileDataArgs);
-  
+
+
